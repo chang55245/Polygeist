92d4c95143e8295bc32a03af4cd2385a01e166f5
chang55245 <lchang21@asu.edu>
2024-03-28 01:39:20 -0700

    add template for taskflow IR

---
+++ include/polygeist/TaskflowDialect.h
@@ -0,0 +1,8 @@
+#ifndef TASKFLOW_DIALECT_H
+#define TASKFLOW_DIALECT_H
+
+#include "mlir/IR/Dialect.h"
+
+#include "polygeist/TaskflowOpsDialect.h.inc"
+
+#endif // TASKFLOW_DIALECT_H
---
+++ usefulcmd.sh
@@ -0,0 +1,3 @@
+# td file
+
+/heorot/lchang21/llvm/Polygeist/llvm-project/build/bin/mlir-tblgen -gen-op-defs /heorot/lchang21/llvm/Polygeist/include/polygeist/TaskflowDialect.td -I /heorot/lchang21/llvm/Polygeist/llvm-project/mlir/include
